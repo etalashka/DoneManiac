7035f85eb2699fde5d5ddd4e95cd767971d32806
Elina Talashka <elt@krks-macbook-pro-2.local>
2019-01-10 21:01:23 +0100

    Initial Commit

---
+++ DoneManiac/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DoneManiac
+//
+//  Created by Elina Talashka on 10/01/2019.
+//  Copyright © 2019 Elina Talashka. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
